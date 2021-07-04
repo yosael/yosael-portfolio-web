632bb6aad083950aad11ab110cd1f48cb18d7fbd
yosael <yosael.gutierrez@gmail.com>
2021-07-03 20:25:30 -0600

    Enhance header and add react-typed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import { Container, Row, Col } from 'reactstrap';
+import Typed from 'react-typed';
+
+const roles = ["Developer","Tech Lover","Team Player","React Js","Angular"];
+
 
 class Index extends React.Component {
   render() {
@@ -37,6 +41,15 @@ class Index extends React.Component {
                     Get informed, collaborate and discover projects I was working on through the years!
                   </h1>
                 </div>
+                <Typed loop 
+                      typeSpeed={70} 
+                      backSpeed={70} 
+                      strings={roles} 
+                      backDelay={1000} 
+                      loopCount={0} 
+                      showCursor 
+                      className="self-typed" 
+                      cursorChar="|" />
                 <div className="hero-welcome-bio">
                   <h1>
                     Let's take a look on my work.
